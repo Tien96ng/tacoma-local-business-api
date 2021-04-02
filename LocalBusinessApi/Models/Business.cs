3478a01f4df08068931f83ccb5d84e2bc54a4dd4
tien96ng <tien96ng@gmail.com>
2021-04-02 10:20:05 -0700

    Create Business.cs Model

---
+++ LocalBusinessApi/Models/Business.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LocalBusiness.Models
+{
+  public class Business
+  {
+    public int BusinessId { get; set; }
+
+    [Required]
+    [StringLength(150)]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string Location { get; set; }
+
+    [Required(ErrorMessage = "Phone number is required")]
+    [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Please enter valid phone number.")]
+    public string PhoneNumber { get; set; }
+    
+    [StringLength(255)]
+    public string WebsiteUrl { get; set; }
+  }
+}
