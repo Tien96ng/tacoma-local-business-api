ed0329ef3e75a00d7afea186e4bfb629cf70f1f9
tien96ng <tien96ng@gmail.com>
2021-04-02 11:18:13 -0700

    Create BusinessesController with full CRUD usage.

---
+++ LocalBusinessApi/Controllers/BusinessesController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LocalBusiness.Models;
+
+namespace LocalBusiness.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BusinessesController : ControllerBase
+  {
+    private readonly LocalBusinessContext _db;
+
+    public BusinessesController(LocalBusinessContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/businesses
+    [HttpGet]
+    public ActionResult<IEnumerable<Business>> Get()
+    {
+      var query = _db.Businesses.AsQueryable();
+
+      return query.ToList();
+    }
+
+    // GET: api/businesses/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Business>> GetBusiness(int id)
+    {
+        var business = await _db.Business.FindAsync(id);
+
+        if (business == null)
+        {
+            return NotFound();
+        }
+
+        return business;
+    }
+
+    // PUT: api/businesses/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Business business)
+    {
+      if (id != business.BusinessId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(business).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!BusinessExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    // POST: api/businesses
+    [HttpPost]
+    public async Task<ActionResult<Business>> Post(Business business)
+    {
+      _db.Businesses.Add(business);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetBusiness), new { id = business.BusinessId }, business);
+    }
+
+    // DELETE: api/businesses/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBusiness(int id)
+    {
+      var business = await _db.Businesses.FindAsync(id);
+      if (business == null)
+      {
+        return NotFound();
+      }
+
+      _db.Businesses.Remove(business);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool BusinessExists(int id)
+    {
+      return _db.Businesses.Any(e => e.business == id);
+    }
+  }
+}
